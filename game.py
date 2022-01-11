7ac965ac401494cc912993d7d66c46815e37d9e6
wilsonBYU <wilsonomar@byui.edu>
2022-01-10 23:32:15 -0600

    Completed printboard and updateboard functions

--- game.py
+++ game.py
@@ -6,11 +6,17 @@ Wilson Romero
 BYU-I 2022
 """
 
-def print_board():
-     pass
+def print_board(board):
+    print(f"\033[1;37m{'|'.join(list(board[:3])):>10}")
+    print(f"{'-'*5:>10}")
+    print(f"{'|'.join(list(board[3:6])):>10}")
+    print(f"{'-'*5:>10}")
+    print(f"{'|'.join(list(board[6:9])):>10}\n")
 
-def update_board():
-     pass
+def update_board(player_symbol, selection, board):
+    sub_board = list(board)
+    sub_board[selection-1] = player_symbol
+    return "".join(sub_board)
      
 def check_win(player, board):
      pass
