ccec87cf3def9ba4bab388beffd61dd92a91475c
wilsoft-gt <wilson.romero1992@gmail.com>
2022-01-10 23:24:18 -0600

    Base file with function names

---
+++ game.py
@@ -0,0 +1,22 @@
+"""
+CSE210
+Solo Code Submission
+TIC-TAC-TOE Prove Assignment
+Wilson Romero
+BYU-I 2022
+"""
+
+def print_board():
+     pass
+
+def update_board():
+     pass
+     
+def check_win(player, board):
+     pass
+    
+def main():
+    pass
+    
+if __name__ == '__main__':
+    main()
